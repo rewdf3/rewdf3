9830a79d46511cb2068e1923c55a440e5b8e88ee
rewdf3 <rewdf334@outlook.com>
2024-06-06 02:32:14 +0100

    Create tgy.py

---
+++ tgy.py
@@ -0,0 +1,25 @@
+/* pages/pictrue/pictrue.wxss */
+swiper{
+  height: 100vh;
+}
+image{
+  width: 100vw;
+  height: 100vh;
+}
+data: {
+    movieList:[{
+      create_time:"2024-5-20 10：14：43",
+      title:"海边顺拍",
+      src:"http://qnm.hunliji.com/bLRuhXGuv29K4zOuKdjdlZJ_9WE=/lh5B36MyVY61V_Iqwvp7I_jpLE6g"
+    },{
+      create_time:"2024-5-20 10：14：43",
+      title:"勿忘安心",
+      src:"http://qnm.hunliji.com/bLRuhXGuv29K4zOuKdjdlZJ_9WE=/lkXpoTgcfRNFrhwkeaeGWGZ1r8uu"
+    },
+    {
+      create_time:"2024-5-20 10：14：43",
+      title:"点滴记忆",
+      src:"http://qnm.hunliji.com/bLRuhXGuv29K4zOuKdjdlZJ_9WE=/lqlkGiHppwT5ikkYZfVr-tGhhR33"
+    }
+  ]
+  },
